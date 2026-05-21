abb7178fdec392fd7c2e45d15902a3219a582c17
Aziz <mohamed-aziz.sahli@utoulouse.fr>
2026-05-21 14:13:03 +0200

    Merge Segment.py and SegmentType.py

--- Scripts/analysis/Segment.py
+++ Scripts/analysis/Segment.py
@@ -3,19 +3,26 @@ Author: Constant ROUX
 Team: SAMoVA
 Date: 2022
 """
+from enum import Enum
+
+
+class SegmentType(Enum):
+    """Represents the segment type of the segment. It can be SILENCE (1) or VOICE (2)."""
+    SILENCE = 1
+    VOICE = 2
 
 
 class Segment:
     """Represents a "segment" of audio frames by the start timecode, the end timecode, the duration / length of the
     segment and hys type (SILENCE or VOICE)."""
 
-    def __init__(self, start, end, duration, type):
+    def __init__(self, start: float, end: float, duration: float, type: SegmentType) -> None:
         self.start = start
         self.end = end
         self.duration = duration
         self.type = type
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (
             str(self.type)
             + " : "
